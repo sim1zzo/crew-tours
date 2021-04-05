b39dee263aed80c1ef1126a9b6e1cd2e880a5be5
Simone Izzo <itausa6@gmail.com>
2021-04-05 18:22:58 +0200

    added mongoose package to the project and created a connection string for mongodb.

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 // MIDDLEWARES
 app.use(express.json());
 
-// TODO
+mongoose.connect('mongodb://localhost/crew-tours', {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true
+}).then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Cannot connect to Mongo', err.message));
 
-console.log("Trying prettier config");
+
+
+
+app.get('/api/tours', (req,res) => {
+  res.status(200).json({
+    status: 'OK',
+    data: {
+      tours
+    }
+  })
+});
+
+app.get('/api/tours/:id', (req, res) => {
+  const tour = parseInt(req.params.id);
+  const tourById = tours.find(t => tour.id === tour.id);
+  if (!tourById)
+    return res
+      .status(404)
+      .json(
+        {
+          status: 'Failed',
+          message: "Invalid ID."
+        });
+  
+  res.status(200).json({
+    status: 'OK',
+    data: {
+      tours
+    }
+  })
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
