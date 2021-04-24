34af56c35ad5d862239b5001700c18b907716320
Simone Izzo <itausa6@gmail.com>
2021-04-24 19:48:22 +0200

    added email for password recovery

--- utils/email.js
+++ utils/email.js
@@ -24,45 +24,6 @@ const sendEmail = async (options) => {
     }
     console.log(info.response);
   });
-
-  // let transport = nodemailer.createTransport({
-  //   host: process.env.EMAIL_Host,
-  //   port: process.env.PORT,
-  //   secure: false,
-  //   auth: {
-  //     user: process.env.EMAIL_USERNAME,
-  //     pass: process.env.EMAIL_PASSWORD,
-  //   },
-  //   // tls: {
-  //   //   rejectUnauthorized: false,
-  //   //   ciphers: 'SSLv3',
-  //   // },
-  // });
-
-  // // verify connection configuration
-  // transport.verify(function (error, success) {
-  //   if (error) {
-  //     console.log(error);
-  //     return;
-  //   } else {
-  //     console.log('Server is ready to take our messages');
-  //   }
-  // });
-
-  // let mailOptions = {
-  //   from: 'Main Admin <admin1@admin.com>',
-  //   to: options.email,
-  //   subject: options.subject,
-  //   text: options.message,
-  //   html: options.html,
-  // };
-  // // console.log(mailOptions);
-  // await transport.sendMail(mailOptions, (error, info) => {
-  //   if (error) {
-  //     return console.log(error);
-  //   }
-  //   console.log('Message sent: %s', info.messageId);
-  // });
 };
 
 module.exports = sendEmail;
