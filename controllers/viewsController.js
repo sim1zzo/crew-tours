da0e3c153d072d58ffb070fff1bc61e185f041ff
Simone Izzo <itausa6@gmail.com>
2021-06-18 22:55:47 +0200

    Added all the users' avatar in users folder. Implemented a route for handle all different routes for each tour. Added Tour View which is nearly compled.

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -8,8 +8,36 @@ exports.getOverview = async (req, res) => {
   });
 };
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'First tour',
+exports.getTour = async (req, res) => {
+  let name = req.params;
+  name = name[0];
+  name = getName(name);
+  const tour = await Tour.findOne({ name: name }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
   });
+  res.status(200).render('tour', {
+    tour,
+  });
+
+  // const tour = await Tour.findOne({ name: name });
 };
+
+function getName(name) {
+  if (name.indexOf('-') !== -1) {
+    name = name.replace('-', ' ');
+    let arr = name.split(' ');
+    let arr2 = [];
+    for (let item of arr) {
+      const first = item[0].toUpperCase();
+      const second = item.slice(1);
+      arr2.push(first + second);
+    }
+    name = arr2.join(' ');
+  } else {
+    const firstLetter = name.charAt(0).toUpperCase();
+    const otherName = name.slice(1);
+    name = firstLetter + otherName;
+  }
+  return name;
+}
